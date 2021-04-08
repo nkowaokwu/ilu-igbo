e8e0757792965e20855db91d5d4b492c9d92f524
Tony Eneh <aeneh90@gmail.com>
2021-04-08 23:55:33 +0100

    Feat: style footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter, faInstagram } from "@fortawesome/free-brands-svg-icons";
 import "./Footer.css";
 
 export function Footer() {
-  return <footer>Footer section</footer>;
+  return (
+    <footer className="text-center p-3">
+      <div>copyright &copy;{new Date().getFullYear()} Ikenga Media</div>
+      <div>
+        Proverbs collated and curated by @nwa_eneh{" "}
+        <a href="https://www.twitter.com/nwa_eneh">
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>{" "}
+        <a href="https://www.instagram.com/nwa_eneh">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+      </div>
+    </footer>
+  );
 }
